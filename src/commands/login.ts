ce533c10b6b545dcd714c5def2fa1e1839fdb576
Anna <16231288+HumbleDeer@users.noreply.github.com>
2026-05-23 07:20:48 +0200

    Improve "/login" cmd main message body
    Rephrases and reordered the message body to improve clarity and relative relevance of sections, as well as operational flow and when a warning/disclaimer works best.

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -5,7 +5,7 @@ import { CommandContext } from "../classes/commandContext";
 const command_data = new SlashCommandBuilder()
     .setName("login")
     .setDMPermission(false)
-    .setDescription(`Adds your token to the database`)
+    .setDescription(`Uses and stores your API key to interact with AI Horde or your account features`)
 
 export default class extends Command {
     constructor() {
@@ -20,11 +20,11 @@ export default class extends Command {
         if(!ctx.database) return ctx.error({error: "The database is disabled. This action requires a database."})
         const add_token_button = new ButtonBuilder({
             custom_id: "save_token",
-            label: "Save Token",
+            label: "🗝️Log in to Stable Horde Bot",
             style: 1
         })
         return ctx.interaction.reply({
-            content: `Add your token by pressing the button below.\nThis is needed to perform actions on your behalf\n\nBy entering your token you agree to the ${await ctx.client.getSlashCommandTag("terms")}\n**You agree to not upload or generate any illegal content**${!ctx.client.config.advanced?.encrypt_token ? "\n\n**The bot is configured not to save your token in an encrypted form!**" : ""}\n\n\nDon't know what the token is?\nCreate an ai horde account here: https://aihorde.net/register`,
+            content: `## Logging in to the bot\nUse the button below to initiate the login process.\nIn the pop-up window, enter your API key and Submit the form.\n-# This process is required to authorize the bot to perform actions on your behalf\n\nYour API key is your personal *(22-character)* key used to make generation requests that use your Kudos balance (and to access your profile if you registered without signing in).\n-#*Don't have an AI Horde account yet? Create one at[AI Horde](https://aihorde.net/register) or read the [FAQ](https://aihorde.net/faq).## Important\nBy logging in with your API key, you agree to the ${await ctx.client.getSlashCommandTag("terms")}\nYou agree to not upload or generate anything prohibited by law or the AI Horde Terms and Conditions, including but not limited to **any** illegal content **whatsoever**.${!ctx.client.config.advanced?.encrypt_token ? "\n\n\n:warning:**Warning!** The bot is configured to save your key **without encryption**. Use caution!" : ""}*`,
             components: [{type: 1, components: [add_token_button.toJSON()]}],
             ephemeral: true
         })
