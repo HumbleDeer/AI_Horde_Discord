a2f41e27c2c60006c78747237d360acd49699d71
Anna <16231288+HumbleDeer@users.noreply.github.com>
2026-05-23 07:33:39 +0200

    Improve "/horde" cmd main message body
    Rephrased and reordered the message body to improve its clarity and minimally elaborated phrasing to better state the core principles.

--- src/commands/horde.ts
+++ src/commands/horde.ts
@@ -5,7 +5,7 @@ import { CommandContext } from "../classes/commandContext";
 const command_data = new SlashCommandBuilder()
     .setName("horde")
     .setDMPermission(false)
-    .setDescription(`Shows info on ai horde`)
+    .setDescription(`A brief introduction to AI Horde`)
 
 export default class extends Command {
     constructor() {
@@ -23,7 +23,7 @@ export default class extends Command {
             color: Colors.Blue,
             title: "AI Horde",
             //TODO: Add more info in the future
-            description: `The [AI Horde](https://https://aihorde.net/) is a crowdsourced service providing Stable Diffusion and other AI services for everyone.\nIt is free, open sourced, and relies on volunteer processing power.\n\nIf you enjoy using it, please consider [onboarding your own PC as a worker](https://github.com/Haidra-Org/AI-Horde-Worker#readme) or supporting its development [on patreon](https://www.patreon.com/db0)${article ? `\n\n**Latest News**\n${article.newspiece}\n<t:${Math.round(Number(new Date(article.date_published!))/1000)}>` : ""}`
+            description: `The [AI Horde](https://https://aihorde.net/) is a crowdsourced volunteer-ran service aimed at providing anyone accessible and fair opportunities to use "generative AI" such as image generation (e.g. Stable Diffusion), text generation (LLMs) and more, free from the roadblocks often faced by socio-economically or otherwise marginalised demographics.\nIt is free, open-sourced, and relies on fully on volunteers offering access to their processing power.\n\nIf you enjoy using it, please consider contributing your computer's resources by [onboarding as a worker](https://github.com/Haidra-Org/AI-Horde-Worker#readme) or supporting its development on [Patreon](https://www.patreon.com/db0)${article ? `\n\n**Latest News**\n${article.newspiece}\n<t:${Math.round(Number(new Date(article.date_published!))/1000)}>` : ""}`
         })
         return ctx.interaction.reply({
             embeds: [embed],
